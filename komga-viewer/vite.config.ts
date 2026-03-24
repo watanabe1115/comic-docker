e0fb829ce7623f082a7ddc74c2796cf9058c466b
watanabe1115 <t.watanabe.1115@gmail.com>
2026-03-25 00:38:10 +0900

    update vite.config

--- komga-viewer/vite.config.ts
+++ komga-viewer/vite.config.ts
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://comic.watanabe1115.com", // ← Komga の URL
-        changeOrigin: true,
-      },
-    },
-  },
+  base: "./",
 })
