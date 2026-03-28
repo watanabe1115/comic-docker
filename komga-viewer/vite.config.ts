2b44848e14aa31e88c0dfa73ce38e98c4b8931f3
watanabe1115 <t.watanabe.1115@gmail.com>
2026-03-28 17:26:11 +0900

    テスト用のproxy設定を追加

--- komga-viewer/vite.config.ts
+++ komga-viewer/vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "./",
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://comic.watanabe1115.com",
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [react()]
+});
